83ee9bcf4707ac8d6d3e8ed321f9bd5bf97bb90d
Ky <caio@cesarleitao.com.br>
2022-06-03 13:29:10 -0300

    Add iOS base files

---
+++ iCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCalc
+//
+//  Created by Caio César on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCalc/iCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalcApp.swift
+//  iCalc
+//
+//  Created by Caio César on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
